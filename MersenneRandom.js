66fddd595df37607bd2154777333796c784a56b9
s0u7a <109493063+s0u7a@users.noreply.github.com>
2023-10-19 19:50:25 +0900

    Create MersenneRandom.js

---
+++ MersenneRandom.js
@@ -0,0 +1,34 @@
+// メルセンヌ・ツイスタのアルゴリズム
+function next(x, a, c, m) {
+  return (x * a + c) % m;
+}
+
+// 乱数生成関数
+function MersenneRandom(seed) {
+  // シード値の生成
+  var x = seed || Math.random() * 2**63 - 1;
+  var s = [
+    Math.random() * 2**63 - 1,
+    Math.random() * 2**63 - 1,
+    Math.random() * 2**63 - 1,
+    Math.random() * 2**63 - 1,
+  ];
+
+  // 攪拌処理
+  for (var i = 0; i < 624; i++) {
+    // 暗号化された乱数を生成
+    var r = window.crypto.getRandomValues(new Uint32Array(1));
+    var r32 = r[0];
+
+    // シード値を更新
+    x = next(x, s[0], s[1], 2**63 - 1);
+    s.push(x);
+    s.shift();
+
+    // シフトレジスタを更新
+    s[0] = r32;
+  }
+
+  // 0 以上 1 未満の乱数を生成
+  return x / (2**63 - 1);
+}
