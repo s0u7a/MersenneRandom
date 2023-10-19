b1515ae07c2e59234c5ea5dd7d2380bb2a30faeb
s0u7a <109493063+s0u7a@users.noreply.github.com>
2023-10-19 23:10:17 +0900

    Update MersenneRandom.js

--- MersenneRandom.js
+++ MersenneRandom.js
@@ -1,7 +1,9 @@
 // Math.Random関数の高速実装 
 // 参考:https://zenn.dev/ame_x/articles/b3ada5021ed174
-let seed = Date.now();
 
+let seed = Date.now();
+const MaxInt64 = 9223372036854776000;
+const MaxInt32 = 2147483647;
 function Xorshift(){seed^=seed<<7;seed^=seed>>>9;return Math.min(1,Math.abs(seed)/2E9)};
 // メルセンヌ・ツイスタのアルゴリズム
 function next(x, a, c, m) {
@@ -11,28 +13,28 @@ function next(x, a, c, m) {
 // 乱数生成関数
 function MersenneRandom(seed) {
   // シード値の生成
-  var x = seed || Xorshift() * 2**63 - 1;
-  var s = [
-    Xorshift() * 2**63 - 1,
-    Xorshift() * 2**63 - 1,
-    Xorshift() * 2**63 - 1,
-    Xorshift() * 2**63 - 1,
-  ];
+  var x = seed || (Xorshift() * MaxInt64);
+  var s0 = Xorshift() * MaxInt64;
+  var s1 = Xorshift() * MaxInt64;
+  var s2 = Xorshift() * MaxInt64;
+  var s3 = Xorshift() * MaxInt64;
 
   // 攪拌処理
   for (var i = 0; i < 624; i++) {
     // 乱数を生成
-    var r32 = Xorshift() * 2**31 - 1;
+    var r32 = Xorshift() * MaxInt32;
 
     // シード値を更新
-    x = next(x, s[0], s[1], 2**63 - 1);
-    s.push(x);
-    s.shift();
+    x = next(x, s0, s1, MaxInt64);
 
     // シフトレジスタを更新
-    s[0] = r32;
+    s0 = s1;
+    s1 = s2;
+    s2 = s3;
+    s3 = r32;
   }
 
   // 0 以上 1 未満の乱数を生成
-  return x / (2**63 - 1);
+  return x / MaxInt64;
 }
+
